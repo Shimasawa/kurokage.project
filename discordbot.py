af420ad963f3ed39fc3bf7ce1e42f61add2a6e71
Shimasawa <71013125+Shimasawa@users.noreply.github.com>
2020-09-29 20:57:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,8 +2,10 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
-      activity=discord.Game("βテスト")
+bot = commands.Bot(
+      command_prefix = "/",
+      activity = discord.Game("βテスト")
+                  )
 token = os.environ.get('DISCODE_BOT_TOKEN')
 
 
