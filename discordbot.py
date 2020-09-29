02f18bdc4b7303e05d66c2018e9b466e9e4f7650
Shimasawa <71013125+Shimasawa@users.noreply.github.com>
2020-09-29 21:04:46 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,11 +4,13 @@ import traceback
 
 bot = commands.Bot(
       command_prefix = "/",
-      activity = discord.Game("βテスト")
       token = os.environ.get('DISCODE_BOT_TOKEN')
-                  )
+           )
 
 @bot.event
+async def on_ready(): # botが起動したときに動作する処理
+    print('ログインしました')
+    await client.change_presence(activity=discord.Game(name="with discord.py", type=1))
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
